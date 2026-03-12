2c3fd75fbc7af4098fbefbb372f68b3af50ea129
Hemarajeshmca <hema@flexicodeindia.com>
2026-03-12 10:00:55 +0530

    Issuesetup hema changes

--- Models/IssueSetupModel.cs
+++ Models/IssueSetupModel.cs
@@ -20,17 +20,18 @@ namespace IPOWeb.Models
 
     public class OfferHeaderModel
     {
-        public string? in_action { get; set; }
-        public string? in_offer_header_gid { get; set; }
-        public string? in_offer_type { get; set; }
-        public string? in_offer_listing { get; set; }
-        public string? in_offer_isin { get; set; }
-        public string? in_offer_status { get; set; }
-        public string? in_offer_remarks { get; set; }
-        public string? in_client_code { get; set; }
-        public string? in_active_status { get; set; }
+        public string? action { get; set; }
+        public string? offer_header_gid { get; set; }
+        public string? offer_code { get; set; }
+        public string? offer_type { get; set; }
+        public string? offer_listing_no { get; set; }
+        public string? offer_isin { get; set; }
+        public string? offer_status { get; set; }
+        public string? offer_remarks { get; set; }
+        public string? client_code { get; set; }
+        public char? active_status { get; set; }
         public string? in_user_code { get; set; }
-
+        public char? delete_flag { get; set; }
 
     }
 }
