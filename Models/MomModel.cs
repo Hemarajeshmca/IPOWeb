54abdcb99d42cbfdd4f9be6b163dc99df3b3b693
venkatesh-2280 <m.venkatesh@flexicodeindia.com>
2026-04-16 15:24:54 +0530

    BOA Screen

--- Models/MomModel.cs
+++ Models/MomModel.cs
@@ -9,6 +9,12 @@ namespace IPOWeb.Models
         public List<BankData> nonasbabankData { get; set; }
         public List<RejectionData> rejectionData { get; set; }
         public List<CategoryData> categoryData { get; set; }
+        public List<CategoryINDData> categoryINDData { get; set; }
+        public List<CategoryCo> categoryCo { get; set; }
+        public List<CategoryNRA10L> categoryNRA10L { get; set; }
+        public List<CategoryNRB10L> categoryNRB10L { get; set; }
+        public List<BankUPIData> bankUPIData { get; set; }
+        public bidApplRcdData bidApplRcd { get; set; }
     }
 
     public class SummaryData
@@ -51,4 +57,62 @@ namespace IPOWeb.Models
         public decimal total { get; set; }
     }
 
-}
+    public class CategoryINDData
+    {
+        public int offer_cat_shares { get; set; }
+        public int total_appl { get; set; }
+        public int total_quantity { get; set; }
+        public decimal? times_subs { get; set; }
+    }
+    public class CategoryCo
+    {
+        public int offer_cat_shares { get; set; }
+        public int total_appl { get; set; }
+        public int total_quantity { get; set; }
+        public decimal? times_subs { get; set; }
+    }
+
+    public class CategoryNRA10L
+    {
+        public int offer_cat_shares { get; set; }
+        public int total_appl { get; set; }
+        public int total_quantity { get; set; }
+        public decimal? times_subs { get; set; }
+    }
+
+    public class CategoryNRB10L
+    {
+        public int offer_cat_shares { get; set; }
+        public int total_appl { get; set; }
+        public int total_quantity { get; set; }
+        public decimal? times_subs { get; set; }
+    }
+
+    public class BankUPIData
+    {
+        public string? bank_name { get; set; }
+        public int no_of_bids { get; set; }
+        public int no_of_shares_applied { get; set; }
+        public long total_amount { get; set; }
+    }
+
+    public class bidApplRcdData
+    {
+        public long asba_total_bids { get; set; }
+        public long asba_total_quantity { get; set; }
+        public long nonasba_total_bids { get; set; }
+        public long nonasba_total_quantity { get; set; }
+        public long upi_total_bids { get; set; }
+        public long upi_total_quantity { get; set; }
+        public long total_bids { get; set; }
+        public long total_quantity { get; set; }
+        public long diff_bids { get; set; }
+        public long diff_quantity { get; set; }
+        public long diff1 { get; set; }
+        public long diff2 { get; set; }
+        public long banknotbidbids { get; set; }
+        public long banknotbidqty { get; set; }
+        public long bank_bids { get; set; }
+        public long bank_bids_qty { get; set; }
+    }
+ }
