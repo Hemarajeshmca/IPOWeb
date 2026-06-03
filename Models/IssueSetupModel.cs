7af79de4f229794cfe1ae1335214a4d23942c7ce
Hemarajeshmca <hema@flexicodeindia.com>
2026-06-03 16:15:28 +0530

    Hema Issue Setup changes

--- Models/IssueSetupModel.cs
+++ Models/IssueSetupModel.cs
@@ -31,6 +31,7 @@ namespace IPOWeb.Models
         public string? offer_remarks { get; set; }
         public string? client_code { get; set; }
         public string? user_code { get; set; }
+        public string? role_code { get; set; }
         public char active_status { get; set; }
         public char delete_flag { get; set; }
 
